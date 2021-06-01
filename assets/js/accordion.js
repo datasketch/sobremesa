c8bf3695376c1e825aedb959869e18172bfb55f6
David Daza <03dazal@gmail.com>
2021-06-01 09:36:00 -0500

    remove accordion transition

--- assets/js/accordion.js
+++ assets/js/accordion.js
@@ -2,13 +2,6 @@ const accordions = Array.from(document.querySelectorAll('.accordion-item'))
 
 accordions.forEach(accordion => {
   accordion.addEventListener('click', function () {
-    const content = this.querySelector('.accordion-content')
-    const accordionIsOpen = this.classList.contains('is-open')
     this.classList.toggle('is-open')
-    if (!accordionIsOpen) {
-      content.style.maxHeight = `${content.getBoundingClientRect().y}px`
-      return
-    }
-    content.style.maxHeight = 0
   })
 })
