180340394eb6fdc1651037acb5cfa83b33ef5851
David Daza <03dazal@gmail.com>
2021-06-02 11:43:51 -0500

    close #4

--- assets/js/accordion.js
+++
@@ -1,7 +0,0 @@
-const accordions = Array.from(document.querySelectorAll('.accordion-item'))
-
-accordions.forEach(accordion => {
-  accordion.addEventListener('click', function () {
-    this.classList.toggle('is-open')
-  })
-})
---
+++ assets/js/services.js
@@ -0,0 +1,5 @@
+import { setupSlider } from './lib/slider'
+import { setupAccordion } from './lib/accordion'
+
+setupAccordion()
+setupSlider('.slider-wrapper')
