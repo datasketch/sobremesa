d829443d65b2855b9532b448723002c6c987a7e2
yoshi <yosman@datasketch.co>
2024-09-13 16:42:32 -0500

    DSPRJT-6630: Ajustes generales página

--- assets/js/services.js
+++ assets/js/services.js
@@ -1,7 +1,25 @@
 import { setupSlider } from './lib/slider'
-import { setupAccordion } from './lib/accordion'
 
-setupAccordion()
+const accordionItem = document.querySelectorAll('.accordion-item')
+const accordionPanel = document.querySelectorAll('.accordion-panel')
+
+accordionItem.forEach((el) => el.addEventListener('click', (e) => {
+  const accordionItemEl = e.target.closest('button')
+  const id = accordionItemEl.getAttribute('data-id')
+  
+  accordionItemEl.classList.toggle('hidden');
+  accordionPanel[id].classList.toggle('hidden')
+}))
+
+accordionPanel.forEach((el) => el.addEventListener('click', (e) => {
+  const accordionPanelEl = e.target.closest('button')
+  const id = accordionPanelEl.getAttribute('data-id')
+  
+  accordionPanel[id].classList.toggle('hidden');
+  accordionItem[id].classList.toggle('hidden')
+}))
+
+
 setupSlider('.slider-wrapper-experiencias', {
   autoplay: {
     delay: 5000
